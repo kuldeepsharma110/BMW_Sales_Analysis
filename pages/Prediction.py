d444d9e74ab85b8c434c194654f64e5f174b8f75
kuldeep <skuldeep110@gmail.com>
2025-11-09 13:38:16 +0530

    Prediction logic updated

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -3,23 +3,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import psycopg2
+import joblib
 
+from bmw_car_sales_eda_app import df
+#from bmw_car_sales_volume_analysis import model_loaded
 
-from bmw_car_sales_eda_app import asia_df
+st.title("PREDICTION OF SALES")
+# st.subheader("ASIA Dataset of Sales")
+# st.dataframe(asia_df)
 
-st.title("PREDICTION OF SALES FOR ASIA")
-st.subheader("ASIA Dataset of Sales")
-st.dataframe(asia_df)
-
-model=st.text_input("Enter car model")
-yr=st.number_input("Enter manufacturing Year")
-color=st.text_input("Enter car color")
-trans_type=st.text_input("Enter car transmission type")
-#region=st.selectbox("Enter the region",asia_df["Region"].unique())
+model=st.selectbox("Enter car model",df["Model"].unique())
+yr=st.selectbox("Enter manufacturing Year",df["Year"].unique())
+color=st.selectbox("Enter car color",df["Color"].unique())
+trans_type=st.selectbox("Enter car transmission type",df["Transmission"].unique())
+region=st.selectbox("Enter the region",df["Region"].unique())
 eng_size=st.number_input("Enter the engine size")
-fuel_type=st.text_input("Enter car fuel type")
+fuel_type=st.selectbox("Enter car fuel type",df["Fuel_Type"].unique())
 
-if st.button("💾 Save Record to Database"):
+if st.button("Predict Sales"):
     # PostgreSQL connection setup
     conn = psycopg2.connect(
         host=st.secrets["postgres"]["host"],
@@ -27,7 +28,7 @@ if st.button("💾 Save Record to Database"):
         user=st.secrets["postgres"]["user"],
         password=st.secrets["postgres"]["password"],
         port=st.secrets["postgres"]["port"]
-    )
+    )    git rm --cached config.toml
 
     cursor = conn.cursor()
 
@@ -40,23 +41,46 @@ if st.button("💾 Save Record to Database"):
         Color VARCHAR(30),
         Fuel_Type VARCHAR(30),
         Transmission VARCHAR(30),
-        Engine_Size_L FLOAT
+        Engine_Size_L FLOAT,
+        Region varchar(30)
     );
     """
     cursor.execute(create_table_query)
     conn.commit()
 
+        # ✅ Create region coloum if not exists
+    create_region_query = """
+    ALTER TABLE asia_sales_records
+    ADD COLUMN IF NOT EXISTS Region VARCHAR(30);
+    """
+    cursor.execute(create_region_query)
+    conn.commit()
+
+
+
+
 
     insert_query = """
-    INSERT INTO asia_sales_records (Model, Year, Color, Fuel_Type, Transmission, Engine_Size_L)
-    VALUES (%s, %s, %s, %s, %s, %s)
+    INSERT INTO asia_sales_records (Model, Year, Color, Fuel_Type, Transmission, Engine_Size_L,Region)
+    VALUES (%s, %s, %s, %s, %s, %s,%s)
     """
 
-    data_tuple = (model, yr, color, fuel_type, trans_type, eng_size)
+    data_tuple = (model, yr, color, fuel_type, trans_type, eng_size,region)
     cursor.execute(insert_query, data_tuple)
 
     conn.commit()
     cursor.close()
     conn.close()
 
-    st.success("✅ Record successfully inserted into PostgreSQL database!")
+    st.write("✅ Record successfully inserted database!")
+
+    # Load the model
+    model_loaded = joblib.load("model.pkl")
+
+    sales_prediction=model_loaded.predict(pd.DataFrame({"Model":[model], "Year":[yr], 
+                                                        "Color":[color], "Fuel_Type":[fuel_type], 
+                                                        "Transmission":[trans_type],
+                                                          "Engine_Size_L":[eng_size],"Region":[region]}))
+    
+    st.success(f"The Prediction for sales is {sales_prediction}")
+
